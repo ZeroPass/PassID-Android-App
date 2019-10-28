f4bfc265fe720e4335151253bf73588959bf01b2
smlu <smlu@s5.net>
2019-10-28 01:30:40 +0100

    Add click listener for register & login buttons

--- app/src/main/java/example/jllarraz/com/passportreader/ui/fragments/MainFragment.kt
+++ app/src/main/java/example/jllarraz/com/passportreader/ui/fragments/MainFragment.kt
@@ -7,15 +7,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.appcompat.widget.AppCompatImageButton
-
 
 import example.jllarraz.com.passportreader.R
 import example.jllarraz.com.passportreader.ui.activities.SelectionActivity
-import example.jllarraz.com.passportreader.ui.activities.SettingsActivity
 
 class MainFragment : androidx.fragment.app.Fragment() {
 
+    private var btnRegister: Button? = null
+    private var btnLogin: Button? = null
     private var btnViewPassport: Button? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -27,9 +26,21 @@ class MainFragment : androidx.fragment.app.Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        btnRegister = view.findViewById(R.id.btnRegister)
+        btnRegister!!.setOnClickListener {
+            val intent = Intent(SelectionActivity.ACTION_REGISTER)
+            startActivity(intent);
+        }
+
+        btnLogin = view.findViewById(R.id.btnLogin)
+        btnLogin!!.setOnClickListener {
+            val intent = Intent(SelectionActivity.ACTION_LOGIN)
+            startActivity(intent);
+        }
+
         btnViewPassport = view.findViewById(R.id.btnViewPassport)
         btnViewPassport!!.setOnClickListener {
-            val intent = Intent(context, SelectionActivity::class.java)
+            val intent = Intent(SelectionActivity.ACTION_VIEW_PASSPORT)
             startActivity(intent);
         }
     }
