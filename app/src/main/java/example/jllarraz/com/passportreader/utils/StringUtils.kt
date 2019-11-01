1278e78f7c3da197dbf2d67ce0c99abc2c39a050
smlu <smlu@s5.net>
2019-11-02 00:39:56 +0100

    Add utility functions to base64  encode/decode byte data

--- app/src/main/java/example/jllarraz/com/passportreader/utils/StringUtils.kt
+++ app/src/main/java/example/jllarraz/com/passportreader/utils/StringUtils.kt
@@ -1,6 +1,7 @@
 package example.jllarraz.com.passportreader.utils
 
 import android.util.Patterns
+import android.util.Base64
 
 object StringUtils {
     private val hexArray = "0123456789ABCDEF".toCharArray()
@@ -17,6 +18,9 @@ object StringUtils {
     fun hexToBytes(hexString: String) = ByteArray(hexString.length / 2) {
         hexString.substring(it * 2, it * 2 + 2).toInt(16).toByte()
     }
+
+    fun b64Encode(bytes: ByteArray) : String = Base64.encodeToString(bytes, Base64.DEFAULT)
+    fun b64Decode(b64String: String): ByteArray = Base64.decode(b64String, Base64.DEFAULT)
 
     fun isValidHttpUrl(url: String): Boolean {
         val valid = Patterns.WEB_URL.matcher(url).matches()
